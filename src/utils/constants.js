9a2af06ad5ad83fda7cfe8c9229f9890f588c000
DeepthiCh111 <deepthichelluboina2002@gmail.com>
2025-06-26 00:25:28 +0530

    second commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const QUERY_SUGGESTIONS =
   "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
-export const API_KEY = "AIzaSyCuRUoiZeZS37jzM02rOLzDK1wHyPQNlyA";
+export const API_KEY = process.env.REACT_API_KEY;
 export const LIVE_API =
   "https://www.googleapis.com/youtube/v3/search?part=snippet&eventType=live&type=video&key=AIzaSyCuRUoiZeZS37jzM02rOLzDK1wHyPQNlyA&maxResults=20&regionCode=IN";
