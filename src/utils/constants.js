01547b6a69ae2c883c477c2204807ec8aad3401b
Chelluboina Deepthi <125189314+DeepthiCh111@users.noreply.github.com>
2025-06-26 15:45:49 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,4 @@
 export const QUERY_SUGGESTIONS =
   "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
-<<<<<<< HEAD
 export const API_KEY = process.env.REACT_APP_API_KEY;
 export const LIVE_API = `https://www.googleapis.com/youtube/v3/search?part=snippet&eventType=live&type=video&key=${API_KEY}&maxResults=20&regionCode=IN`;
-=======
-export const API_KEY = process.env.REACT_API_KEY;
-export const LIVE_API =
-  "https://www.googleapis.com/youtube/v3/search?part=snippet&eventType=live&type=video&key={API_KEY}&maxResults=20&regionCode=IN";
->>>>>>> 6d0c03709a2b4e963c2e6cba86b608142da6d9ed
