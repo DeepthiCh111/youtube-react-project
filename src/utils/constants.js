1a9d01984cd2ea2b4c7f2294271cfd273385f6c3
DeepthiCh111 <deepthichelluboina2002@gmail.com>
2025-06-26 15:35:27 +0530

    Fixed API key env issue and update constants.js commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,4 @@
 export const QUERY_SUGGESTIONS =
   "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
-export const API_KEY = process.env.REACT_API_KEY;
-export const LIVE_API =
-  "https://www.googleapis.com/youtube/v3/search?part=snippet&eventType=live&type=video&key=AIzaSyCuRUoiZeZS37jzM02rOLzDK1wHyPQNlyA&maxResults=20&regionCode=IN";
+export const API_KEY = process.env.REACT_APP_API_KEY;
+export const LIVE_API = `https://www.googleapis.com/youtube/v3/search?part=snippet&eventType=live&type=video&key=${API_KEY}&maxResults=20&regionCode=IN`;
